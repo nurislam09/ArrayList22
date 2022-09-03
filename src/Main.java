5d737d217d1a229b451657f547806894a083dd99
nurislam09 <akinatorxxl@gmail.com>
2022-09-03 13:10:49 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayeven = new ArrayList<>();
+        ArrayList<Integer> arrayodd = new ArrayList<>();
+
+        Random random = new Random();
+        for (int i = 0; i <=50 ; i++) {
+            int a = random.nextInt(1,100);
+            arrayList.add(a);
+            if (a%2==0){
+                arrayeven.add(a);
+            }else {
+                arrayodd.add(a);
+            }
+        }
+        System.out.println(" Random 50 numbers:"+ arrayList);
+        System.out.println("Even numbers:" +arrayeven );
+        System.out.println("Odd numbers:"+arrayodd);
+    }
+}
